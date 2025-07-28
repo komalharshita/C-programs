60555097f4c330d7a72a498a321e2feda82253e9
Komal Harshita <komal.sony234@gmail.com>
2025-07-28 20:19:32 +0530

    added files via upload

---
+++ 2. operators/trial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	
+	int a,b,c ;
+	
+	printf("Enter 1st number to multiply: ");
+	scanf("%d", &a);
+	
+	printf("Enter 2nd number to multiply: ");
+	scanf("%d", &b);
+	
+	c=a*b ;
+	
+	printf("The result is: %d", &c);
+		
+return 0;
+
+}
